cfcc45ea11fb2326b70fd27d4534b6374710d092
nitodeco <n.moehn@outlook.com>
2024-08-10 12:06:02 +0200

    chore: Updated configs and installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/renderer/index.html', './src/renderer/src/**/*.{svelte,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    }
+  },
+  plugins: []
+}
